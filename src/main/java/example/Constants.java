69729b8b2377208c9506ac6ae01918274d8996d9
Anton Khmara <anton_khmara@mail.ru>
2021-11-21 15:16:54 +0300

    add promo

--- src/main/java/example/Constants.java
+++ src/main/java/example/Constants.java
@@ -3,5 +3,5 @@ package example;
 import java.util.List;
 
 public interface Constants {
-	List<String> PROMO_GOODS = List.of("beetroot", "garlic", "apple");
+	List<String> PROMO_GOODS = List.of("beetroot", "garlic", "apple", "orangeg");
 }
